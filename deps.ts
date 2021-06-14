fea9fe9676841d741d32c146fcb82729cb6d6cb2
Tiago Gomes <t.dfgomes@icloud.com>
2021-06-14 23:46:23 +0100

    chore: change external deps cdn

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export {
-  parse,
-  validateSchema,
-  validate,
   execute,
-} from "https://cdn.pika.dev/graphql@^15.0.0";
+  parse,
+  validate,
+  validateSchema,
+} from "https://esm.sh/graphql";
 
-export type { IExecutableSchemaDefinition } from "https://cdn.pika.dev/@graphql-tools/schema@^6.0.0"; 
-export { makeExecutableSchema } from "https://cdn.pika.dev/@graphql-tools/schema@^6.0.0";
+export type { IExecutableSchemaDefinition } from "https://esm.sh/@graphql-tools/schema";
+export { makeExecutableSchema } from "https://esm.sh/@graphql-tools/schema";
